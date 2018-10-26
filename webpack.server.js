55f01d34508aee61658a2fc02d9cb9237f702801
Michael Wolowyk <volovikm2@gmail.com>
2018-10-26 11:40:27 +0200

    Initial commit

---
+++ webpack.server.js
@@ -0,0 +1,39 @@
+const path = require('path');
+
+module.exports = {
+    // inform webpack building a bundle for node js,
+    // rather than for the browser
+
+    target: 'node',
+
+    // tell webpack the root file for our
+    // server application
+
+    entry: './src/index.js',
+
+    // tell webpack where to put output file
+    // that is generated
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+
+    // Tell webpack to run babel on every file it runs through
+
+    module: {
+        rules: [
+            {
+                test: /\.js?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: [
+                        'react',
+                        'stage-0',
+                        ['env', {targets: {browsers: ['last 2 versions']}}]
+                    ]
+                }
+            }
+        ]
+    }
+};
